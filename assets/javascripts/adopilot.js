2705867b6e57fd7fe65e80ab42ff09a19c944048
Admir Hodžić <adopilot@gmail.com>
2014-07-17 17:51:44 +0200

    jos jedan
    jos jedanom dodao da vidim hoce sli se

--- assets/javascripts/adopilot.js
+++ assets/javascripts/adopilot.js
@@ -4,7 +4,7 @@ Discourse.Dialect.inlineBetween({
   rawContents: true,
   emitter: function(contents) {
     var list = Discourse.Dialect.cook(contents, {});
-    return "<iframe src='http://172.16.0.54/vbox-msg/701/INBOX/msg0007.wav' style='border-width:0' frameborder='0' scrolling='no' width='100%'></iframe>"
+    return ["<iframe src='http://172.16.0.54/vbox-msg/701/INBOX/msg0007.wav' style='border-width:0' frameborder='0' scrolling='no' width='100%'></iframe>"];
 	
   }
 });
