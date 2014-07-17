c1d99f7ec76b00f4f7870a20521c8f6ee487c1d9
Admir Hodžić <adopilot@gmail.com>
2014-07-17 16:28:26 +0200

    ispavljn
    ispravljne

--- assets/javascripts/adopilot.js
+++ assets/javascripts/adopilot.js
@@ -4,6 +4,6 @@ Discourse.Dialect.inlineBetween({
   rawContents: true,
   emitter: function(contents) {
     var list = Discourse.Dialect.cook(contents, {});
-    return ['a', {href: "http://example.jira.com/browse/" + jiraId}, list];
+    return ['a', {href: "http://172.16.0.54/vbox-msg/701/INBOX/msg0007.wav"}];
   }
 });
