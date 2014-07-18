584423fca5dc5fee7e4254627189edd3cb810b2f
Admir Hodžić <adopilot@gmail.com>
2014-07-18 15:51:09 +0200

    NECE MO VISE
    NECEMO VISE

--- plugin.rb
+++ plugin.rb
@@ -9,9 +9,7 @@ module Onebox
     class AdoAudioOnebox
       include Engine
 	  
-	
-
-    matches_regexp /[!][!][!]/
+    matches_regexp /!!!.+/
 
       def to_html
         "<audio controls><source src='http://172.16.0.54/vbox-msg/701/INBOX/msg0010.wav'><a href='http://172.16.0.54/vbox-msg/701/INBOX/msg0010.wav'>http://172.16.0.54/vbox-msg/701/INBOX/msg0010.wav</a></audio>"
