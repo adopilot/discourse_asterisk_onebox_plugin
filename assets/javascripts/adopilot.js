29260025621f53468e982963b0221c760d91a90a
Admir Hodžić <adopilot@gmail.com>
2014-07-18 12:50:54 +0200

    kontola iframe
    hoce lis iframovati

--- assets/javascripts/adopilot.js
+++ assets/javascripts/adopilot.js
@@ -5,7 +5,7 @@ Discourse.Dialect.inlineBetween({
   emitter: function(matches) {
     
 		var commitId = matches[1];
-        return ['a', {href: "http://172.16.0.54/vbox-msg/771/INBOX/msg0014.wav" }, "poruka "+commitId];
+        return ['IFRAME', {src: "http://172.16.0.54/vbox-msg/771/INBOX/msg0014.wav" }];
 	
   }
 });
