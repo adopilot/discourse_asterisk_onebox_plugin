2384080c89f929cb59ec72d627aad6caefdd5d08
Admir Hodžić <adopilot@gmail.com>
2014-07-18 15:42:36 +0200

    SA USKLICNICIMA
    SA USKLICNICAIMA

--- plugin.rb
+++ plugin.rb
@@ -11,7 +11,7 @@ module Onebox
 	  
 	
 
-    matches_regexp [\$][\$][\$]
+    matches_regexp [!][!][!]
 
       def to_html
         "<audio controls><source src='http://172.16.0.54/vbox-msg/701/INBOX/msg0010.wav'><a href='http://172.16.0.54/vbox-msg/701/INBOX/msg0010.wav'>http://172.16.0.54/vbox-msg/701/INBOX/msg0010.wav</a></audio>"
