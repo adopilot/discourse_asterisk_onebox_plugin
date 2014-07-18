aa57750e1188628d9bef9731233685eaf74edb21
Admir Hodžić <adopilot@gmail.com>
2014-07-18 15:33:50 +0200

    ado
    ado

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,6 @@
 # about: This plugin have task to try onbox  imamo one box koliko imamo 
 # version: 0.1
 # authors: Admir Hodžić
-
 #register_asset "javascripts/adopilot.js", :server_side
 
 module Onebox
