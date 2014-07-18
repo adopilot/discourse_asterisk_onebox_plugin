4065ed83fa6b621b405872e09bac49c305acbe10
Admir Hodžić <adopilot@gmail.com>
2014-07-18 15:30:12 +0200

    ado
    ado

--- plugin.rb
+++ plugin.rb
@@ -10,16 +10,9 @@ module Onebox
     class AdoAudioOnebox
       include Engine
 	  
-	re1='.*?'	# Non-greedy match on filler
-	re2='(?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)'	# Uninteresting: month
-	re3='.*?'	# Non-greedy match on filler
-	re4='((?:Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Sept|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?))'	# Month 1
-	re5='.*?'	# Non-greedy match on filler
-	re6='(\\$[0-9]+(?:\\.[0-9][0-9])?)(?![\\d])'	# Dollar Amount 1
+	
 
-	re=(re1+re2+re3+re4+re5+re6)
-
-    matches_regexp re
+    matches_regexp (\\$[0-9]+(?:\\.[0-9][0-9])?)(?![\\d])
 
       def to_html
         "<audio controls><source src='http://172.16.0.54/vbox-msg/701/INBOX/msg0010.wav'><a href='http://172.16.0.54/vbox-msg/701/INBOX/msg0010.wav'>http://172.16.0.54/vbox-msg/701/INBOX/msg0010.wav</a></audio>"
