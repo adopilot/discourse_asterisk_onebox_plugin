3f25959549706abb2f5d5b549d2f48fcafb7c977
Admir Hodžić <adopilot@gmail.com>
2014-07-18 12:13:08 +0200

    opet proba
    proba tona

--- assets/javascripts/adopilot.js
+++ assets/javascripts/adopilot.js
@@ -2,9 +2,10 @@ Discourse.Dialect.inlineBetween({
   start: '$$$',
   stop: '$$$',
   rawContents: true,
-  emitter: function(contents) {
-    var list = Discourse.Dialect.cook(contents, {});
-    return ["<iframe src='http://172.16.0.54/vbox-msg/701/INBOX/msg0007.wav' style='border-width:0' frameborder='0' scrolling='no' width='100%'></iframe>"];
+  emitter: function(matches) {
+    
+		var commitId = matches[1];
+        return ['a', {href: "http://172.16.0.54/vbox-msg/771/INBOX/msg0008.wav" }];
 	
   }
 });
