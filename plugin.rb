03c6f2c899c12385024c5de719fb380998e1af20
Admir Hodžić <adopilot@gmail.com>
2014-07-19 15:42:18 +0200

    ado
    ado king kong

--- plugin.rb
+++ plugin.rb
@@ -13,7 +13,7 @@ module Onebox
       matches_regexp /^https?:\/\/(?:www\.)?google\.com\/calendar\/embed\?.+/
 
       def to_html
-        "<iframe src='#{@url}' style='border-width:0' frameborder='0' scrolling='no' width='100%'></iframe>"
+        "<iframe src='#{@url}' style='border-width:0' frameborder='0' scrolling='no' width='100%' height='600' ></iframe>"
       end
     end
   end
