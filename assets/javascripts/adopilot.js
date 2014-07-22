0d017f04b8eb84eb7609f9f702bc5140b88a557b
Admir Hodžić <adopilot@gmail.com>
2014-07-22 11:29:49 +0200

    nv

--- assets/javascripts/adopilot.js
+++ assets/javascripts/adopilot.js
@@ -5,8 +5,8 @@ Discourse.Dialect.inlineBetween({
   emitter: function() {
     
 	
-	var ado = "<audio controls><source src='http://172.16.0.54/vbox-msg/701/INBOX/msg0007.wav'><a href='http://172.16.0.54/vbox-msg/701/INBOX/msg0007.wav>http://172.16.0.54/vbox-msg/701/INBOX/msg0007.wav</a></audio>";
+	return ['span', "<audio controls><source src='http://172.16.0.54/vbox-msg/701/INBOX/msg0007.wav'><a href='http://172.16.0.54/vbox-msg/701/INBOX/msg0007.wav>http://172.16.0.54/vbox-msg/701/INBOX/msg0007.wav</a></audio>"];
 	
-	return ado;
+	
   }
 });
