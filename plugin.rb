77a0032611bca3ffb85e809f2f05d3bac3a5d314
Admir Hodžić <adopilot@gmail.com>
2014-07-22 10:24:48 +0200

    sa
    sa

--- plugin.rb
+++ plugin.rb
@@ -27,9 +27,7 @@ module Onebox
         "<b>#{@folder} hoces ili neces da pastas ovo #{@msgNum}</b>"
       end
 	  
-      def ado
-         "<audio controls><source src='http://172.16.0.54/vbox-msg/701/INBOX/msg0000.wav'><a href='http://172.16.0.54/vbox-msg/701/INBOX/msg0000.wav'>http://172.16.0.54/vbox-msg/701/INBOX/msg0000.wav</a></audio>"
-      end
+
 	  
     end
   end
