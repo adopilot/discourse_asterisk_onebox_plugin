486b6517c88a3cbd91a736d5169860019a5610d0
Admir Hodžić <adopilot@gmail.com>
2014-07-22 08:35:31 +0200

    ado nc
    ado nc

--- plugin.rb
+++ plugin.rb
@@ -15,16 +15,16 @@ module Onebox
       REGEX = /http:\/\/asterix\.robot\.ba\/(\d+)\/(\d+)\//
 	  matches_regexp REGEX
 	  
-	  def folder
+	  def @folder
 		@url.match(REGEX)[1]
 	  end
 	  
-	  def msgNum
+	  def @msgNum
 	    @url.match(REGEX)[2]
 	  end
 	  
       def to_html		
-        "<b>Folder: #{folder} ado  Msg num: #{msgNum}</b>"
+        "<b>Folder: #{@folder} ado  Msg num: #{@msgNum}</b>"
       end
 	  
     end
