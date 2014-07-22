124bbc4e4b9d058d0bd17144ac008736bba4fafb
Admir Hodžić <adopilot@gmail.com>
2014-07-22 09:41:04 +0200

    nv
    vnv

--- plugin.rb
+++ plugin.rb
@@ -15,10 +15,9 @@ module Onebox
       REGEX = /http:\/\/asterix\.robot\.ba\/(\d+)\/(\d+)\//
 	  matches_regexp REGEX
 	  
-
 	  
       def to_html		
-        "<b>Folder: king kong</b>"
+         "<audio controls><source src='http://172.16.0.54/vbox-msg/701/INBOX/msg0000.wav'><a href='http://172.16.0.54/vbox-msg/701/INBOX/msg0000.wav'>http://172.16.0.54/vbox-msg/701/INBOX/msg0000.wav</a></audio>"
       end
 	  
     end
